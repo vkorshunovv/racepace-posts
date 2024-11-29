33ee9ba83e6d0b42f2b9ec7b654d4a1092f7264a
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-11-29 13:22:57 +0100

    create RacePost model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class RacePost(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.TextField(blank=True, null=True)
+    distance = models.DecimalField(max_digits=5, decimal_places=2)
+    elevation = models.IntegerField()
+    date = models.DateField(auto_now_add=True)
+    
