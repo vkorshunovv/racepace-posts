b6a67ff27f03f482a34ce95cca9f79e328b24563
Viktor Korshunov <viktorkorshunov@MacBook-Air-Viktor.local>
2024-11-29 13:29:37 +0100

    create a view

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import RacePost
+from .serializers import RacePostSerializer
 
 # Create your views here.
+
+class RacePostViewSet(viewsets.ModelViewSet):
+    queryset = RacePost.objects.all()
+    serializer_class = RacePostSerializer
